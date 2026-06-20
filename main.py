cc16bf2ea175ab4ca05b2dd57c9ed94a41809443
NonSentientExistence <david@venngren.se>
2026-06-20 15:54:02 +0200

    added C -> F conversion func. While loop for finished app

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+# var for while loop condition. While loop will run until running != True
+running = True
 
-# Copied function from provided lab code
-# Added correct conversion F -> C
-def fahrenheit_to_celsius(t):
-    t_celsius = (t - 32) * 5/9
-    return t_celsius
+while(running == True):
 
-svar = input('Ange en temperatur i Fahrenheit: ')
-t_fahrenheit = float(svar)
-t = fahrenheit_to_celsius(t_fahrenheit)
-print("Celcius: ", t)
+    # Copied function from provided lab code
+    # Added correct conversion F -> C
+    """
+    def fahrenheit_to_celsius(t):
+        t_celsius = (t - 32) * 5/9
+        return t_celsius
+    """
+    
+    ## Convert C -> F function
+    def celsius_to_fahrenheit(t):
+        t_fahrenheit = (t * 9/5) + 32
+        return t_fahrenheit
 
+    """
+    svar = input('Ange en temperatur i Fahrenheit: ')
+    t_fahrenheit = float(svar)
+    t = fahrenheit_to_celsius(t_fahrenheit)
+    print("Celcius: ", t)
+    """
+
+    svar = input('Ange en temperatur i Celsius: ')
+    t_celsius = float(svar)
+    t = celsius_to_fahrenheit(t_celsius)
+    print("Fahrenheit: ", t)
+
+    # Stop while loop, hardcoded until correctly implemented
+    running = False
